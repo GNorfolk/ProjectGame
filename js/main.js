ca900731affaa100c26d9cae0a7fbf8cbe6aeea2
GNorfolk <georgenorfolk@hotmail.co.uk>
2017-09-18 10:25:41 +0100

    Added end state for object format.

--- js/main.js
+++ js/main.js
@@ -7,4 +7,57 @@ $(function() {
 		$(this).closest('#mainDiv').children('#unitSelect').removeClass('mainHidden');
 	});
 
+	var unitP1Stat = {};
+	var unitP2Stat = {};
+
+	// var unitP1Stat = {
+	// 	unit1: {
+	// 		name: '1-1',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit2: {
+	// 		name: '1-2',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit3: {
+	// 		name: '1-3',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit4: {
+	// 		name: '1-1',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit5: {
+	// 		name: '1-1',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit6: {
+	// 		name: '1-1',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit7: {
+	// 		name: '1-1',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	},
+	// 	unit8: {
+	// 		name: '1-1',
+	// 		type: 'swordsmen',
+	// 		health: 100;
+	// 	}
+	// };
+
+	var selectButton = $('#topSelect button')
+	selectButton.on('click', function(event) {
+		console.log('Second button working');
+		$(this).closest('#unitSelect').addClass('mainHidden');
+		$(this).closest('#mainDiv').children('#stats').removeClass('mainHidden');
+	})
+
 });
--- readme.md
+++ readme.md
@@ -4,11 +4,12 @@
 ~~Add mechanic calculations~~
 ~~Add boilerplate for instruction main component~~
 ~~Add boilerplate for selection comp~~
-Add boilerplate for statistics and results
-Add object model js for unit stats
+~~Add boilerplate for statistics and results~~
+~~Add object model js for unit stats~~
 Add selection js click events
 Add basic boilerplate for attacks
 Add icons for different unit types
+Update styling for stats
 
 Interaction mechanics:
 Swordsmen	+50% 	v Spearmen
